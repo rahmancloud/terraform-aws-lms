6be7e9efeb98276b4edfa87153278618afd9c77b
faizzamzuri <155245976+faizzamzuri@users.noreply.github.com>
2024-07-19 11:19:23 +0800

    Create EC2 and NSG Rules (#4)
    * Create main.tf
    
    create main.tf with bitnami aws ami
    
    * Update main.tf
    
    * Create variables.tf
    
    * associate NSG to instance and change ingress cidr

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+  resource "aws_instance" "Bitnami_LMS" {
+  ami           = "ami-0bbdb939ba1170846"
+  instance_type = "t2.micro"
+
+    vpc_security_group_ids = [aws_security_group.LMS_SG.id]
+
+  tags = {
+    Name = "Bitnami_LMS"
+  }
+}
+
+resource "aws_security_group" "LMS_SG" {
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "LMS_SG"
+    Project = "Bitnami_LMS"
+  }
+}
+
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0f8d97b8b295ad42d"
+}
