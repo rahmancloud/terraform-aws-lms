e8e2cd000c6284bb35c685d39b71f42a77fdbd7b
Rahman <amanyus@gmail.com>
2024-07-19 12:58:31 +0800

    devcontainer & fmt (#5)
    * Create terraform.yml
    
    * Create Dockerfile
    
    * Rename Dockerfile to .devcontainer/Dockerfile
    
    * Create devcontainer.json
    
    * tf fmt

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-  resource "aws_instance" "Bitnami_LMS" {
+resource "aws_instance" "Bitnami_LMS" {
   ami           = "ami-0bbdb939ba1170846"
   instance_type = "t2.micro"
 
-    vpc_security_group_ids = [aws_security_group.LMS_SG.id]
+  vpc_security_group_ids = [aws_security_group.LMS_SG.id]
 
   tags = {
     Name = "Bitnami_LMS"
